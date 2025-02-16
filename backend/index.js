a19fbd3d3879ed837e65d5dc249df95cbcc7e632
Vikram Chiluka <vikramchiluka18@gmail.com>
2025-02-16 15:48:40 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
